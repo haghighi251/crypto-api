7699b94bf2b5de1b082e77e672752e862fe2ae81
Amir <haghighi251@gmail.com>
2023-12-21 21:53:16 +0100

    feat: Add OpenAPI to the api endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Add OpenAPI configuration to enable API documentation and client generation.
+  const config = new DocumentBuilder()
+    .setTitle('CryptoHiveNet example')
+    .setDescription('The CryptoHiveNet API description')
+    .setVersion('1.0')
+    .addTag('Crypto')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8000);
 }
 bootstrap();
